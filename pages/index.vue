db2af7d05f5f0eb2625ceb76b88567793113e85a
Salu Alhassan <alhassansalu1@gmail.com>
2024-07-28 11:53:11 +0000

    update on table

--- pages/index.vue
+++ pages/index.vue
@@ -24,7 +24,7 @@
                 <p class="text-gray-400 text-center md:w-[600px]">
                     Collective responsibility that requires efforts from individuals, communities, businesses, and governments.
                 </p class="mt-2">
-                <Button> learn more </Button>
+                <Button> Learn More </Button>
             </div>
         </div>
         <div class="flex flex-col md:flex-row justify-center items-center gap-[18px] mt-24">
@@ -41,5 +41,20 @@
                 <h2 class="text-xl font-bold text-[#185755]">Nature</h2>
             </div>
         </div>
+        <div class="flex flex-col md:flex-row px-[18px] md:px-[450px] justify-center items-center md:justify-between mt-[18px]">
+            <h1 class="text-gray-600 text-4xl font-extrabold text-center md:text-start">Our initiatives for the 2024</h1>
+            <p class="text-gray-400 text-center md:text-end w-[300px]">Find out the projects that we are undertaking this year.</p>
+        </div>
+        <div class="flex justify-center mt-[24px]"> 
+            <UContainer>
+                <Table />
+            </UContainer>           
+            
+        </div>
     </div>
 </template>
+
+
+<script>
+
+</script>
