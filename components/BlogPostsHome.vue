203f035d37d1177ef51d8ee60efa42510a975187
Salu Alhassan <alhassansalu1@gmail.com>
2024-08-09 05:43:30 +0000

    update

--- components/BlogPostsHome.vue
+++ components/BlogPostsHome.vue
@@ -26,9 +26,9 @@ export default {
       currentPage: 1,
       postsPerPage: 5,
       posts: [
-        { id: 1, title: 'Drought', date: '2024-08-04', content: "<p class='text-gray-400'>This is the first post content.</p>", image: 'https://cdn.pixabay.com/photo/2016/04/13/02/01/climate-change-1325882_1280.jpg' },
-        { id: 2, title: 'Air Pollution', date: '2024-08-03', content: "<p class='text-gray-400'>This is the second post content.</p>", image: 'https://cdn.pixabay.com/photo/2022/11/06/23/39/climate-change-issue-7575216_1280.jpg' },
-        { id: 2, title: 'Desertification', date: '2024-08-03', content: "<p class='text-gray-400'>This is the second post content.</p>", image: 'https://cdn.pixabay.com/photo/2012/11/06/15/40/tree-64310_1280.jpg' },
+        { id: 1, title: 'Drought', date: '2024-08-04', content: "<p class='text-gray-400'>Drought is a prolonged period of abnormally low rainfall, leading to a shortage of water... </p>", image: 'https://cdn.pixabay.com/photo/2016/04/13/02/01/climate-change-1325882_1280.jpg' },
+        { id: 2, title: 'Air Pollution', date: '2024-08-03', content: "<p class='text-gray-400'>Air pollution refers to the presence of harmful substances in the atmosphere....</p>", image: 'https://cdn.pixabay.com/photo/2022/11/06/23/39/climate-change-issue-7575216_1280.jpg' },
+        { id: 2, title: 'Desertification', date: '2024-08-03', content: "<p class='text-gray-400'>Desertification refers to the process by which fertile land becomes desert...</p>", image: 'https://cdn.pixabay.com/photo/2012/11/06/15/40/tree-64310_1280.jpg' },
         // Add more posts here
       ]
     }
